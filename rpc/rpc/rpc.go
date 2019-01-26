7ef8106a1bfe10a19c53e60f4b1ab43b62a3e4d9
hao <hao@infinitystones.io>
2019-01-26 23:55:05 +0000

    Backup RPC interface

--- rpc/rpc/rpc.go
+++ rpc/rpc/rpc.go
@@ -1,90 +1,27 @@
 package rpc
 
+/*
+ * Gorilla RPC example: https://gist.github.com/dbehnke/10437286
+ */
+
 import (
-    "os"
-    "fmt"
-    "bytes"
-    "strings"
-    "io/ioutil"
     "net/http"
-    "encoding/json"
 )
 
-type URLType struct {
-    URL string `json:"url"`
-    Source string `json:"source"`
-    Method string `json:"method"`
-    Data string `json:"data"`
-    ContentType string `json:"content_type"`
-    Params []string `json:"params"`
+type Service struct{}
+
+/*
+ * Backup RPC
+ */
+type BackupArgs struct{}
+
+type BackupReply struct {
+  Message string
 }
 
-type ConfigType struct {
-    Root string `json:"root"`
-    Endpoint string `json:"endpoint"`
-    Protocol string `json:"protocol"`
-    URLs []URLType `json:"URLs"`
+func (h *Service) Backup(r *http.Request, args *BackupArgs, reply *BackupReply) error {
+	reply.Message = "Succeed"
+	return nil
 }
 
-var Config map[string]ConfigType
 
-func (config *ConfigType) ReadConfigFile(filename string) {
-    jsonFile, err := os.Open(filename)
-    if err != nil {
-        fmt.Println(err)
-    }
-    defer jsonFile.Close()
-
-    byteValue, _ := ioutil.ReadAll(jsonFile)
-
-    json.Unmarshal([]byte(byteValue), config)
-}
-
-func Get(config ConfigType, route URLType, context map[string]string) []byte {
-    source := route.Source
-    for param, value := range context {
-        pattern := "$" + param
-        source = strings.Replace(source, pattern, value, -1)
-    }
-    url := fmt.Sprintf("%s://%s%s", config.Protocol, config.Endpoint, source)
-    resp, _ := http.Get(url)
-    defer resp.Body.Close()
-    body, _ := ioutil.ReadAll(resp.Body)
-    return body
-}
-
-func Post(config ConfigType, route URLType, context map[string]string) []byte {
-    source := route.Source
-    data_str := route.Data
-    for param, value := range context {
-        pattern := "$" + param
-	source = strings.Replace(source, pattern, value, -1)
-	data_str = strings.Replace(data_str, pattern, value, -1)
-    }
-    url := fmt.Sprintf("%s://%s%s", config.Protocol, config.Endpoint, source)
-    data := []byte(data_str)
-    resp, _ := http.Post(url, route.ContentType, bytes.NewBuffer(data))
-    defer resp.Body.Close()
-    body, _ := ioutil.ReadAll(resp.Body)
-    return body
-}
-
-func RegisterConfig(key string, config_file string) {
-    var config ConfigType
-
-    config.ReadConfigFile(config_file)
-    Config[key] = config
-}
-
-func Initialize() {
-    config_dir := "../../config/"
-    var keys = []string{
-        "tezos",
-	"eos",
-	"gxchain"}
-
-    Config = make(map[string]ConfigType)
-    for _, key := range keys {
-        RegisterConfig(key, config_dir + key + ".json")
-    }
-}
--- rpc/server/server.go
+++ rpc/server/server.go
@@ -3,7 +3,37 @@ package main
 import (
     "../rpc"
     "github.com/gin-gonic/gin"
+    gorilla_rpc "github.com/gorilla/rpc"
+    "github.com/gorilla/rpc/json"
 )
+
+var Server *gorilla_rpc.Server
+
+func RegisterService(receiver interface{}, name string) error {
+    return Server.RegisterService(receiver, name)
+}
+
+func GetServer() *gorilla_rpc.Server {
+    if Server != nil {
+        return Server
+    }
+
+    Server := gorilla_rpc.NewServer()
+    Server.RegisterCodec(json.NewCodec(), "application/json")
+    Server.RegisterCodec(json.NewCodec(), "application/json;charset=UTF-8")
+
+    handler := new(rpc.Service)
+    Server.RegisterService(handler, "")
+    return Server
+}
+
+func makeContext(c *gin.Context, params []string, ctxt map[string]string) {
+    if params != nil {
+        for _, param := range params {
+            ctxt[param] = c.Param(param)
+	}
+    }
+}
 
 func RegisterRoutes(router *gin.Engine) {
     for _, config := range rpc.Config {
@@ -13,12 +43,8 @@ func RegisterRoutes(router *gin.Engine) {
 	    // GET method
 	    if route.Method == "" || route.Method == "get" {
 	        router.GET(url, func(c *gin.Context) {
-		    context := make(map[string]string)
-                    if route.Params != nil {
-                        for _, param := range route.Params {
-			    context[param] = c.Param(param)
-			}
-	            }
+                    context := make(map[string]string)
+		    makeContext(c, route.Params, context)
 		    c.Writer.Write(rpc.Get(config, route, context))
 		})
 	    }
@@ -26,15 +52,18 @@ func RegisterRoutes(router *gin.Engine) {
 	    // POST method
 	    if route.Method == "post" {
 	        router.POST(url, func(c *gin.Context) {
-		    context := make(map[string]string)
-                    if route.Params != nil {
-                        for _, param := range route.Params {
-			    context[param] = c.Param(param)
-			}
-		    }
+                    context := make(map[string]string)
+		    makeContext(c, route.Params, context)
 		    c.Writer.Write(rpc.Post(config, route, context))
 		})
 	    }
+
+	    // RPC calls
+	    if route.Method == "rpc" {
+	        router.POST(url, func(c *gin.Context) {
+		    GetServer().ServeHTTP(c.Writer, c.Request)
+		})
+            }
         }
     }
 }
