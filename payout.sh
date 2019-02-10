b768c475527d49b400a7812c89e0401ae33dece0
Hao <hao@infinitystones.io>
2019-02-10 06:04:26 +0000

    script that launches payout

---
+++ payout.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function usage() {
+    echo "Usage: ./payout.out [reset|start]"
+}
+
+if [ $# -ne 1 ]; then
+    usage
+    exit 1
+fi
+
+case "$1" in
+    "reset")
+        rm $HOME/tezos/.payout_records
+	touch $HOME/tezos/.payout_records
+	;;
+    "start")
+        cd $HOME/monitor-server/app/tezos
+	go run payout/*.go
+	;;
+    *)
+        usage
+	;;
+esac
